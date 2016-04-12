acb404fd9259aa70a46fc0407fd4d6c22a263ac7
Shawn Mehan <shawn.mehan@ieee.org>
2016-04-12 10:54:05 -0700

    Clean up imports. Deal with some PEP warnings. Better exception handling for HTTP requests. DRY - extracted method _make_request from search and details methods.

--- src/Gmap/map_api.py
+++ src/Gmap/map_api.py
@@ -5,17 +5,19 @@
 Class that will make a search for nearby places of a particular type given a lat, long
 """
 
-from loggerUtils import init_logging
+# standard modules
 import logging
-import yaml
 import json
 import time
-from gmaps import Geocoding, client
 from urllib.request import Request
 from urllib.request import urlopen
+# 3rd party modules
+import yaml
+# application modules
+from loggerUtils import init_logging
 
 
-class Gmap():
+class Gmap(object):
     """
     """
 
@@ -23,7 +25,7 @@ class Gmap():
         """Constructor for Gmap"""
         init_logging()
         self.logger = logging.getLogger()
-        with open("Gmap/google_config.yml", 'r') as f:  #  TODO: needs to look in same dir
+        with open("Gmap/google_config.yml", 'r') as f:  # TODO: needs to look in same dir
             settings = yaml.load(f)
         self.api_key = settings['GOOGLE_API_KEY']
         self.search_type = settings['GOOGLE_SEARCH_METHOD']
@@ -34,14 +36,17 @@ class Gmap():
         :param place_id:
         :return:
         """
-        resultsList = []
         r = self._get_details(place_id)
+        if r is None:
+            self.logger.warn('Failure to return details from google API'
+                             ' for {}'.format(place_id))
+            return []
         if r['status'] == 'OVER_QUERY_LIMIT':
             self.logger.warn("Google quota exceeded. Cool down!")
             return []
-        resultsList = r['result']
-        self.logger.info("Google details fetched for %s" % resultsList['name'])
-        return resultsList
+        results_list = r['result']
+        self.logger.info("Google details fetched for %s" % results_list['name'])
+        return results_list
 
     def fetch_results(self, coords, rad=1000):
         """
@@ -51,26 +56,46 @@ class Gmap():
         :param rad: radius of search
         :return: list of json results
         """
-        resultsList = []
         r = self._search_google(coords[0], coords[1], rad)
+        if r is None:
+            self.logger.warn('Failure to return search from google API'
+                             ' for {lat}, {long}'.format(lat=coords[0],
+                                                         long=coords[1]))
+            return []
         if r['status'] == 'OVER_QUERY_LIMIT':
             self.logger.warn("Google quota exceeded. Cool down!")
             return []
+        results_list = []
         for place in r['results']:
-            resultsList.append(place)
+            results_list.append(place)
         if 'next_page_token' in r:
             time.sleep(2)
             while True:
                 npt = r['next_page_token']
                 r = self._search_google(coords[0], coords[1], rad, npt)
                 for place in r['results']:
-                    resultsList.append(place)
+                    results_list.append(place)
                 try:
                     r['next_page_token']
                 except:
                     break
         self.logger.info("Google maps searched and results returned for %s, %s." % (coords[0], coords[1]))
-        return resultsList, self.search_type
+        return results_list, self.search_type
+
+    def _make_request(self, request_url):
+        """
+        Builds a HTTP Request object and opens and reads data from the object.
+        :param request_url: URL to use for request
+        :return: json payload from URL
+        """
+        req = Request(request_url)
+        with urlopen(req) as rh:
+            try:
+                response = json.loads(rh.read().decode('utf-8'))
+                return response
+            except Exception as e:
+                self.logger.error(e)
+                return None
 
     def _get_details(self, place_id):
         """
@@ -79,23 +104,20 @@ class Gmap():
         :return:
         """
         url = self._build_details_url(place_id)
-        req = Request(url)
-        r = json.loads(urlopen(req).read().decode('utf-8'))
-        return r
+        return self._make_request(url)
 
-    def _search_google(self, lat, long, r, npt=None):
+    def _search_google(self, lat, long, radius, npt=None):
         """
-        Builds and executes a REST API for searching places, with pagination control
+        Builds and executes a REST API call
+        for searching places, including param for pagination control
         :param lat:
         :param long:
-        :param r:
-        :param npt:
+        :param radius: raidus to use for radar search
+        :param npt: next page token from multi-page search result
         :return:
         """
-        url = self._build_search_url(lat, long, radius=r, npt=npt)
-        req = Request(url)
-        r = json.loads(urlopen(req).read().decode('utf-8'))
-        return r
+        url = self._build_search_url(lat, long, radius=radius, npt=npt)
+        return self._make_request(url)
 
     def _build_details_url(self, place_id):
         """
@@ -132,8 +154,8 @@ class Gmap():
         url += ','
         url += str(long)
         url += '&radius='
-        url += str(radius)
-        url += '&types=car_dealer'
+        url += str(radius)  # TODO: types and keywords need to be parameterized
+        url += '&types=car_dealer'  # types=laundry
         #url += '&keyword=laundromat'  # keyword: "(cinema) OR (theater)" unclear if it works
         url += '&key='
         url += self.api_key
